349ea749b75f6d86ced3c373c5c528ff2ca5398a
Daniel Ames <satsuk81@googlemail.com>
2022-08-09 16:46:02 +0100

    Update AEB-RebuildHyperVVM.ps1

--- AEB-RebuildHyperVVM.ps1
+++ AEB-RebuildHyperVVM.ps1
@@ -12,7 +12,6 @@ Build Version   : v2
 
 .LINK
 More Info       : https://github.com/Automation-PowerShell/AzureEnvironmentBuilder
-
 #>
 
 Param(
@@ -22,15 +21,13 @@ Param(
 #region Setup
 Set-Location $PSScriptRoot
 
-    # Dot Source Variables
-#. .\ScriptVariables.ps1
-#. .\ClientLoadVariables.ps1
-
-#$azSubscription = "743e9d63-59c8-42c3-b823-28bb773a88a6" # Visual Studio Professional
-$azSubscription = "7660dc8a-b807-45fd-817f-a5df6f70204b" # Visual Studio Professional Subscription 2
+#$azSubscription = "743e9d63-59c8-42c3-b823-28bb773a88a6"       # Visual Studio Professional
+$azSubscription = "7660dc8a-b807-45fd-817f-a5df6f70204b"        # Visual Studio Professional Subscription 2
 $VaultName = "keyvault-dames10"
 $Domain = "space"
 $OUPath = "OU=Workstations,OU=Computers,OU=Space,DC=space,DC=dan"
+$StorageAccountName = "storageeucpackaging10"                   # Storage account name (if used) (24 chars maximum)
+$StorageAccountKey = '4sdj3IetnnAjfCLHCDOvkrnxa8HMIZ2Hy72K80b78QbDWg7oDPk1CtJOJOQb4SQBJQMt47pjwE/d+ASteI38Gw=='
 
 $HyperVLocalAdminUser = "administrator"
 $DomainJoinUser = "wella\svc_PackagingDJ"
@@ -42,7 +39,7 @@ $EventlogSource = "Hyper-V VM Rebuild Script"                   # Event Log Sour
 
 $VMDrive = "F:"                                                 # Specify the root disk drive to use
 $VMFolder = "Hyper-V"
-$VMMachineFolder = "Virtual Machines"                                   # Specify the folder to store the VM data
+$VMMachineFolder = "Virtual Machines"                           # Specify the folder to store the VM data
 $VHDFolder = "Virtual Hard Disks"                               # Specify the folder to store the VHDs
 $VMCheckpointFolder = "Checkpoints"                             # Specify the folder to store the Checkpoints
 $VmNamePrefix = "EUC-UAT-"
@@ -52,12 +49,6 @@ $VMCPUCount = 4
 $VMSwitchName = "Packaging Switch"
 Try {$VMHostIP = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "Ethernet").IPAddress}
 Catch {$VMHostIP = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "Ethernet 2").IPAddress}
-
-    # For Static IP Configuration
-#$IPAddress = ""
-#$IPSubnetPrefix = "24"
-#$IPGateway = "192.168.0.1"
-#$IPDNS = @("10.21.224.10","10.21.224.11","10.21.239.196")
 
 # Create Error Trap
 trap {
@@ -79,17 +70,6 @@ Install-Module -Name Az.Storage,Az.KeyVault -Force -ErrorAction Stop
 Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Information -Message "Attempting to connect to Azure"
 Connect-AzAccount -identity -ErrorAction Stop -Subscription $azSubscription
 
-Set-Location $PSScriptRoot
-#$VMListData = Import-Csv .\hyperv-vms.csv       # needs improving.
-
-##$adminPassword = (Get-AzKeyVaultSecret -VaultName $VaultName -Name "HyperVLocalAdmin").SecretValue
-#$adminCred = New-Object System.Management.Automation.PSCredential ($HyperVLocalAdminUser, $adminPassword)
-#$LocalCred = $adminCred
-#$DomainCredPassword = (Get-AzKeyVaultSecret -VaultName $VaultName -Name "DomainUser").SecretValue
-#$DomainCred = New-Object System.Management.Automation.PSCredential ($DomainUserUser, $DomainCredPassword)
-#$DomainJoinPassword = (Get-AzKeyVaultSecret -VaultName $VaultName -Name "DomainJoin").SecretValue
-#$DomainJoinCred = New-Object System.Management.Automation.PSCredential ($DomainJoinUser, $DomainJoinPassword)
-
 # Get Passwords from KeyVault
 $LocalAdminPassword = (Get-AzKeyVaultSecret -VaultName $VaultName -Name "HyperVLocalAdmin").SecretValue
 $LocalAdminCred = New-Object System.Management.Automation.PSCredential ($HyperVLocalAdminUser, $LocalAdminPassword)
@@ -97,13 +77,6 @@ $DomainJoinPassword = (Get-AzKeyVaultSecret -VaultName $VaultName -Name "DomainJ
 $DomainJoinCred = New-Object System.Management.Automation.PSCredential ($DomainJoinUser, $DomainJoinPassword)
 $DomainUserPassword = (Get-AzKeyVaultSecret -VaultName $VaultName -Name "DomainUser").SecretValue
 $DomainUserCred = New-Object System.Management.Automation.PSCredential ($DomainUserUser, $DomainUserPassword)
-
-#if($LocalCred) {Remove-Variable LocalCred;$LocalCred = Get-Credential -Credential $LocalCredUser;$LocalCred | Export-CliXml -Path .\HyperVLocalAdmin.xml} else{$LocalCred = Get-Credential -Credential $LocalCredUser;$LocalCred | Export-CliXml -Path .\HyperVLocalAdmin.xml}
-#if($DomainCred) {Remove-Variable DomainCred;$DomainCred = Get-Credential -Credential $DomainCredUser;$DomainCred | Export-CliXml -Path .\DomainUser.xml} else{$DomainCred = Get-Credential -Credential $DomainCredUser;$DomainCred | Export-CliXml -Path .\DomainUser.xml}
-#if($DomainJoinCred) {Remove-Variable DomainJoinCred;$DomainJoinCred = Get-Credential -Credential $DomainJoinUser;$DomainJoinCred | Export-CliXml -Path .\DomainJoin.xml} else{$DomainJoinCred = Get-Credential -Credential $DomainJoinUser;$DomainJoinCred | Export-CliXml -Path .\DomainJoin.xml}
-#$LocalCred = Import-CliXml -Path .\HyperVLocalAdmin.xml
-#$DomainCred = Import-CliXml -Path .\DomainUser.xml
-#$DomainJoinCred = Import-CliXml -Path .\DomainJoin.xml
 
 if(!$DomainUserCred) {
     exit
@@ -158,38 +131,16 @@ function Create-VM {
     $VMObject | Set-VM -ProcessorCount $VMCPUCount
     $VMObject | Set-VM -StaticMemory
     $VMObject | Set-VM -AutomaticCheckpointsEnabled $false
-    $VMObject | Set-VM -SnapshotFileLocation "$VMDrive\Hyper-V\$VMCheckpointFolder"
+    $VMObject | Set-VM -SnapshotFileLocation "$VMDrive\$VMFolder\$VMCheckpointFolder"
     $VMObject | Add-VMHardDiskDrive -Path $VMDrive\$VMFolder\$VHDFolder\$VMName\$VMName.vhdx
 
     $Date = Get-Date -Format yyyy-MM-dd
     $Time = Get-Date -Format HH:mm
     $VMObject | Checkpoint-VM -SnapshotName "Base Config ($Date - $Time)"
-
     $VMObject | Start-VM -Verbose -ErrorAction Stop
-    Start-Sleep -Seconds 720
-
-    #$IPAddress = ($VMListData | Where-Object {$_.Name -eq $VMName}).IPAddress
 
         # Pre Domain Join
-    Remove-Variable erroric -ErrorAction SilentlyContinue
-    Invoke-Command -VMName $VMName -Credential $LocalAdminCred -ErrorVariable erroric -ScriptBlock {
-        #$NetAdapter = Get-NetAdapter -Physical | where {$_.Status -eq "Up"}
-        #if (($NetAdapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
-        #    $NetAdapter | Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false
-        #}
-        #if (($NetAdapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
-        #    $NetAdapter | Remove-NetRoute -AddressFamily IPv4 -Confirm:$false
-        #}
-        #$NetAdapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $Using:IPAddress -PrefixLength $Using:IPSubnetPrefix -DefaultGateway $Using:IPGateway | Out-Null
-        #$NetAdapter | Set-DnsClientServerAddress -ServerAddresses $Using:IPDNS | Out-Null
-        #Start-Sleep -Seconds 60
-        #if(!(Test-Connection $VMHostIP -Quiet)) { Write-Error "Networking Issue" }
-        #if(!(Test-Connection "google.com" -Quiet)) { Write-Error "DNS Issue" }
-    }
-    if($erroric) {
-        Write-Error $error[0]
-        Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Error -Message $error[0].Exception
-    }
+    Start-Sleep -Seconds 90
     Remove-Variable erroric -ErrorAction SilentlyContinue
     Invoke-Command -VMName $VMName -Credential $LocalAdminCred -ErrorVariable erroric -ScriptBlock {
         Get-AppxPackage -Name Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
@@ -199,8 +150,37 @@ function Create-VM {
         Write-Error $error[0]
         Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Error -Message $error[0].Exception
     }
-
     Start-Sleep -Seconds 90
+    Remove-Variable erroric -ErrorAction SilentlyContinue
+    Invoke-Command -VMName $VMName -Credential $LocalAdminCred -ErrorVariable erroric -ScriptBlock {
+            # Enable Remote Desktop
+        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+        Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+        net localgroup "Remote Desktop Users" /add "Domain Users"
+
+            # Autopilot Hardware ID
+        New-Item -Type Directory -Path "C:\HWID" -Force
+        Set-Location -Path "C:\HWID"
+        Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -ErrorAction Stop
+        Install-PackageProvider -Name NuGet -Force -ErrorAction Stop
+        Install-Script -Name Get-WindowsAutoPilotInfo -Force -ErrorAction Stop
+        Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
+
+            # Map Packaging Share
+        cmd.exe /C cmdkey /add:`"$Using:StorageAccountName.file.core.windows.net`" /user:`"Azure\$Using:StorageAccountName`" /pass:`"$Using:StorageAccountKey`"
+        New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$Using:StorageAccountName.file.core.windows.net\pkgazfiles01" -Persist
+
+            # Upload AutoPilotHWID
+        mkdir -Path "Z:\EUC Applications\Packaging Environment Build Files\Autpilot IDs" -Name $env:COMPUTERNAME -Force
+        Copy-Item -Path "C:\HWID\AutoPilotHWID.csv" -Destination "Z:\EUC Applications\Packaging Environment Build Files\Autpilot IDs\$env:COMPUTERNAME" -Force
+    }
+    if($erroric) {
+        Write-Error $error[0]
+        Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Error -Message $error[0].Exception
+    }
+
+        # Join Domain
+    Start-Sleep -Seconds 10
     Remove-Variable erroric -ErrorAction SilentlyContinue
     Invoke-Command -VMName $VMName -Credential $LocalAdminCred -ErrorVariable erroric -ScriptBlock {
         Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
@@ -228,42 +208,13 @@ function Create-VM {
         Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Error -Message $error[0].Exception
     }
 
-        # Post Domain Join - LocalCred wont work anymore.
+        # Post Domain Join
     Start-Sleep -Seconds 90
     Remove-Variable erroric -ErrorAction SilentlyContinue
     Invoke-Command -VMName $VMName -Credential $DomainUserCred -ErrorVariable erroric -ScriptBlock {
         Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
         Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-        #netsh advfirewall firewall add rule name="allow RemoteDesktop" dir=in protocol=TCP localport=3389 action=allow
-        #New-NetFirewallRule -DisplayName "Restrict_RDP_access" -Direction Inbound -Protocol TCP -LocalPort 3389 -RemoteAddress 192.168.1.0/24,192.168.2.100 -Action Allow
-        #Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
         net localgroup "Remote Desktop Users" /add "Domain Users"
-
-            # Autopilot Hardware ID
-        New-Item -Type Directory -Path "C:\HWID" -Force
-        Set-Location -Path "C:\HWID"
-        Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -ErrorAction Stop
-        Install-PackageProvider -Name NuGet -Force -ErrorAction Stop
-        Install-Script -Name Get-WindowsAutoPilotInfo -Force -ErrorAction Stop
-        Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
-
-            # Map Packaging Share
-        $source = $source = "X:\EUC Applications\Packaging Environment Build Files\Prevision"
-        cmd.exe /C cmdkey /add:`"wlprodeusprodpkgstr01.file.core.windows.net`" /user:`"Azure\wlprodeusprodpkgstr01`" /pass:`"s9joA/NOc94meybF/lasFWDB55fFx3JP0OEYddtYljpRswpjErowqz9wXZe2zVY1/CW1Aujle3ED1fEJtnsEvg==`"
-        New-PSDrive -Name X -PSProvider FileSystem -Root "\\wlprodeusprodpkgstr01.file.core.windows.net\pkgazfiles01" -Persist
-        Copy-Item -Path $source\MapDrv.ps1 -Destination "C:\Users\Public\Desktop" -Force
-        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "MapPackagingDrive" -Value "Powershell.exe -ExecutionPolicy Unrestricted -file `"C:\Users\Public\Desktop\MapDrv.ps1`"" -PropertyType "String"
-
-            # Upload AutoPilotHWID
-        mkdir -Path "X:\EUC Applications\Packaging Environment Build Files\Autpilot IDs" -Name $env:COMPUTERNAME -Force
-        Copy-Item -Path "C:\HWID\AutoPilotHWID.csv" -Destination "X:\EUC Applications\Packaging Environment Build Files\Autpilot IDs\$env:COMPUTERNAME" -Force
-
-            # Add Windows Capibilities Back In
-        #Add-WindowsCapability -Online -Name Microsoft.Windows.PowerShell.ISE~~~~0.0.1.0
-        #Add-WindowsCapability -Online -Name App.StepsRecorder~~~~0.0.1.0
-        #Add-WindowsCapability -Online -Name Microsoft.Windows.Notepad~~~~0.0.1.0
-        #Add-WindowsCapability -Online -Name Microsoft.Windows.MSPaint~~~~0.0.1.0
-        #Add-WindowsCapability -Online -Name Microsoft.Windows.WordPad~~~~0.0.1.0
     }
     if($erroric) {
         Write-Error $error[0]
@@ -277,13 +228,16 @@ function Create-VM {
     $MACAddress = $VMObject.NetworkAdapters.MacAddress
     $IPAddress = (Get-DhcpServerv4Scope | Get-DhcpServerv4Lease | Where-Object {($_.ClientId -replace "-") -eq $MACAddress}).IPAddress.IPAddressToString
     if(!(Get-NetNatStaticMapping -NatName LocalNAT | Where-Object {$_.ExternalPort -like "*$VMNumber"})) {
-        Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0" -ExternalPort 50$VMNumber -InternalIPAddress $IPAddress -InternalPort 3389 -NatName LocalNAT -Protocol TCP -ErrorAction Stop | Out-Null
+        Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0" -ExternalPort 55$VMNumber -InternalIPAddress $IPAddress -InternalPort 3389 -NatName LocalNAT -Protocol TCP -ErrorAction Stop | Out-Null
+    }
+    else {
+        Get-NetNatStaticMapping -NatName LocalNAT | Where-Object {$_.ExternalPort -like "*$VMNumber"} | Remove-NetNatStaticMapping -Confirm:$false | Out-Null
+        Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0" -ExternalPort 55$VMNumber -InternalIPAddress $IPAddress -InternalPort 3389 -NatName LocalNAT -Protocol TCP -ErrorAction Stop | Out-Null
     }
 }
 
 #region Main
 Write-Output "Running AEB-RebuildHyperVVM.ps1"
-
 if($RVMVMName -eq "") {
     $VMList = Get-VM -Name * | Select-Object Name, Uptime, State
     #$VMList = $VMListData
@@ -298,3 +252,47 @@ Create-VM -VMName $RVMVMName
 Get-NetNatStaticMapping | Select-Object StaticMappingID,ExternalIPAddress,ExternalPort,InternalIPAddress,InternalPort | Export-CSV -Path ".\netnatmapping.csv" -Force -NoTypeInformation
 Write-Output "Completed AEB-RebuildHyperVVM.ps1"
 #endregion Main
+
+#region Old Code
+    # Dot Source Variables
+#. .\ScriptVariables.ps1
+#. .\ClientLoadVariables.ps1
+
+    # For Static IP Configuration
+#$IPAddress = ""
+#$IPSubnetPrefix = "24"
+#$IPGateway = "192.168.0.1"
+#$IPDNS = @("10.21.224.10","10.21.224.11","10.21.239.196")
+
+    # Static IP Addresses
+#$NetAdapter = Get-NetAdapter -Physical | where {$_.Status -eq "Up"}
+#if (($NetAdapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+#    $NetAdapter | Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false
+#}
+#if (($NetAdapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+#    $NetAdapter | Remove-NetRoute -AddressFamily IPv4 -Confirm:$false
+#}
+#$NetAdapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $Using:IPAddress -PrefixLength $Using:IPSubnetPrefix -DefaultGateway $Using:IPGateway | Out-Null
+#$NetAdapter | Set-DnsClientServerAddress -ServerAddresses $Using:IPDNS | Out-Null
+#Start-Sleep -Seconds 60
+#if(!(Test-Connection $VMHostIP -Quiet)) { Write-Error "Networking Issue" }
+#if(!(Test-Connection "google.com" -Quiet)) { Write-Error "DNS Issue" }
+
+    # Remote Desktop Config
+#netsh advfirewall firewall add rule name="allow RemoteDesktop" dir=in protocol=TCP localport=3389 action=allow
+#New-NetFirewallRule -DisplayName "Restrict_RDP_access" -Direction Inbound -Protocol TCP -LocalPort 3389 -RemoteAddress 192.168.1.0/24,192.168.2.100 -Action Allow
+#Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
+
+    # Add Windows Capibilities Back In
+#Add-WindowsCapability -Online -Name Microsoft.Windows.PowerShell.ISE~~~~0.0.1.0
+#Add-WindowsCapability -Online -Name App.StepsRecorder~~~~0.0.1.0
+#Add-WindowsCapability -Online -Name Microsoft.Windows.Notepad~~~~0.0.1.0
+#Add-WindowsCapability -Online -Name Microsoft.Windows.MSPaint~~~~0.0.1.0
+#Add-WindowsCapability -Online -Name Microsoft.Windows.WordPad~~~~0.0.1.0
+
+    # Map Packaging Share
+#$source = "X:\EUC Applications\Packaging Environment Build Files\Prevision"
+#Copy-Item -Path $source\MapDrv.ps1 -Destination "C:\Users\Public\Desktop" -Force
+#New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "MapPackagingDrive" -Value "Powershell.exe -ExecutionPolicy Unrestricted -file `"C:\Users\Public\Desktop\MapDrv.ps1`"" -PropertyType "String"
+
+#endregion Old Code
