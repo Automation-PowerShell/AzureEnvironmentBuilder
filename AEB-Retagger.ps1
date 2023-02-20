7be9c80c9d9acb6eeb5d2e8e23a160cc50615eba
Daniel Ames <satsuk81@googlemail.com>
2023-02-20 11:48:02 +0000

    2023-02-20 -- Update 1
    * Improvements to Retagger function
    * Added IPAM to toolkit

--- AEB-Retagger.ps1
+++ AEB-Retagger.ps1
@@ -39,29 +39,23 @@ if ($devops) {
   # ...
 }
 else {
-  ConnectTo-Azure
+  #ConnectTo-Azure
+  Connect-AzAccount -Tenant $clientSettings.azTenant
 }
+#endregion
 
-$mergedTags = @{
-  'Application' = 'AEB';
-}
+#$csvfile = './subscriptions-resourcegroups.csv'
+#$csvpath = Split-Path -Path $csvfile -Parent
+#$Import = Import-Csv -Path $csvfile
+#$subscriptions = $Import.Sub | Sort-Object -Unique
+#$selectedSub = $subscriptions | Out-GridView -OutputMode Single
 
-#$mergedTags = @{
-#  'Application' = 'AEB';
-#  'AEB-Client' = 'TestClient1'
-#  'AEB-Environment' = 'Dev'
-#}
-
-$csvfile = './subscriptions-resourcegroups.csv'
-$csvpath = Split-Path -Path $csvfile -Parent
-$Import = Import-Csv -Path $csvfile
-$subscriptions = $Import.Sub | Sort-Object -Unique
-
-$selectedSub = $subscriptions | Out-GridView -OutputMode Single
+$selectedSub = Get-AzSubscription -TenantId $clientSettings.azTenant | Out-GridView -OutputMode Single -Title 'Select Subscription'
 Write-Host "##vso[task.LogIssue type=warning;]Switching Subscription to $($selectedSub)"
-Select-AzSubscription -Subscription $selectedSub | Out-Null
-$rgs = Get-AzResourceGroup | Select-Object ResourceGroupName
-$selectedRG = $rgs | Out-GridView -OutputMode Single
+Select-AzSubscription -Subscription $selectedSub -Tenant $clientSettings.azTenant | Out-Null
+
+$rgs = Get-AzResourceGroup | Select-Object ResourceGroupName | Sort-Object -Property ResourceGroupName
+$selectedRG = $rgs | Out-GridView -OutputMode Single -Title 'Select Resource Group'
 
 $resources = Get-AzResource -ResourceGroupName $selectedRG.ResourceGroupName
 $selectedResources = ($resources | Select-Object -Property @(
@@ -71,11 +65,28 @@ $selectedResources = ($resources | Select-Object -Property @(
     @{Name = 'AEB-Application'; Expression = { ($_.Tags.'AEB-Application') } }
     @{Name = 'AEB-Client'; Expression = { ($_.Tags.'AEB-Client') } }
     @{Name = 'AEB-Environment'; Expression = { ($_.Tags.'AEB-Environment') } }
-    'Id' ) | Sort-Object -Property Name | Out-GridView -OutputMode Multiple)
+    'Id' ) | Sort-Object -Property Name | Out-GridView -OutputMode Multiple -Title 'Select Resources to Retag')
+
+
+$selectedTags = (Get-AzResourceGroup -ResourceGroupName $selectedRG.ResourceGroupName).Tags | Out-GridView -OutputMode Multiple -Title 'Select Tag Pairs'
+$mergedTags = @{}
+foreach ($tag in $selectedTags) {
+  $mergedTags.Add($tag.Name, $tag.Value)
+}
 
 foreach ($id in $selectedResources.Id) {
-  Update-AzTag -ResourceId $id -Tag $mergedTags -Operation Merge -Verbose
+  Update-AzTag -ResourceId $id -Tag $mergedTags -Operation Merge | Select-Object *
 }
+
+$resources = Get-AzResource -ResourceGroupName $selectedRG.ResourceGroupName
+$selectedResources = ($resources | Select-Object -Property @(
+    'Name'
+    @{Name = 'Appliction'; Expression = { ($_.Tags.Application) } }
+    @{Name = 'Environment'; Expression = { ($_.Tags.Environment) } }
+    @{Name = 'AEB-Application'; Expression = { ($_.Tags.'AEB-Application') } }
+    @{Name = 'AEB-Client'; Expression = { ($_.Tags.'AEB-Client') } }
+    @{Name = 'AEB-Environment'; Expression = { ($_.Tags.'AEB-Environment') } }
+    'Id' ) | Sort-Object -Property Name | Out-GridView -OutputMode None -Title 'Review Resources')
 
 #$rgID = (Get-AzResourceGroup -ResourceGroupName $selectedRG).ResourceId
 #Update-AzTag -ResourceId $rgID -Tag $mergedTags -Operation Merge -Verbose
